899fedf7019ad37a6048eee47d1aec689d9b127a
lusineep <60401284+lusineep@users.noreply.github.com>
2020-02-18 16:45:26 +0400

    Create homework3.js

---
+++ homework3.js
@@ -0,0 +1,85 @@
+//1
+//2
+function getNumbers(allArray){
+            let newArrayOdd = [];
+            let newArrayEven = [];
+            for(let i = 0; i < allArray.length; i++){
+                      if(typeof((allArray[i])) === "number" || allArray[i] === 0 ){
+                            if(allArray[i]%2 === 0){
+                               newArrayOdd.push(allArray[i]);
+                        }else{
+                            newArrayEven.push(allArray[i]);
+                        }
+                  }
+        }
+               for(let i = 0; i < newArrayOdd.length; i++ ){
+                        newArrayEven.push(newArrayOdd[i]);
+                }
+              // let result = newArrayEven.concat(newArrayOdd);
+             //         return result;
+                return newArrayEven;
+}
+
+//3
+function numStringCount(string){
+               let countN = 0;
+               let countS = 0;
+               for(let i = 0; i < string.length; i++){
+                    if(typeof(string[i]) === "number"){
+                        countN ++;
+                    }else if(typeof(string[i]) === "string"){
+                        countS ++;
+                    }
+
+            }
+            let result = Number: ${countN} , String: ${countS};
+            return result;
+    }
+
+//4
+function maxlength(words){
+                let min = 9;
+                let max = 0;
+           for(let i = 0; i < words.length; i++){
+                
+                if(words[i].length < min){
+                    
+                    min = words[i].length;
+              
+                }
+                if(words[i].length > max){
+                    
+                    max = words[i].length;
+              
+                }
+               
+           
+        }
+ let result  = max + min;  
+return result;      
+    }
+
+//5
+function sumOfNum(numbersArray){
+            let result = 0;
+            for(let i = 0; i < numbersArray.length; i++){
+debugger;
+                    result += numbersArray[i];
+                      }
+        return result;
+}
+
+//6
+
+function productsOfNum(numbersArray){
+            let result = [];
+            for(let i = 0; i < numbersArray.length; i++){
+debugger;
+                    if(numbersArray[i+1] || numbersArray[i+1] === 0){
+                    let product = numbersArray[i] * numbersArray[i+1];
+                    result.push(product);
+                    
+                    }
+                      }
+        return result;
+}
